31508061c177e472ac47338d7848c770ee3ae8e9
nehru35 <sonehru@gmail.com>
2022-07-04 12:12:28 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("browser").textContent = platform.name;
+document.getElementById("version").textContent = platform.version;
+document.getElementById("layout").textContent = platform.layout;
+document.getElementById("os").textContent = platform.os;
+document.getElementById("description").textContent = platform.description;
