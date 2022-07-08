adc6006ecbde6f75ac9cc574f39bf347a2d61f61
nehru35 <sonehru@gmail.com>
2022-07-08 12:11:03 +0100

    Updated the script
    Created a function to store all the proccess, added the window.sreen to get info about the screean

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-document.getElementById("browser").textContent = platform.name;
-document.getElementById("version").textContent = platform.version;
-document.getElementById("layout").textContent = platform.layout;
-document.getElementById("os").textContent = platform.os;
-document.getElementById("description").textContent = platform.description;
+function displayInfo() {
+
+    let screen = window.screen;
+
+    document.getElementById("browser").textContent = platform.name;
+    document.getElementById("version").textContent = platform.version;
+    document.getElementById("layout").textContent = platform.layout;
+    document.getElementById("os").textContent = platform.os;
+    document.getElementById("description").textContent = platform.description;
+    document.getElementById("height").textContent = screen.height + " px";
+    document.getElementById("width").textContent = screen.width + " px";
+}
+
+
+displayInfo();
